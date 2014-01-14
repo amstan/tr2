dd7c4f9b8b26950ff7fa7677c5b0c9648a0d7085
Alexandru Stan <alex@hypertriangle.com>
2014-01-13 19:41:24 -0500

    perfect moving

--- python/stepper/virtual.py
+++ python/stepper/virtual.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import time
 import math
-modes = {"idle","move","run"}
 
 class StepperModel(object):
 	"""Class meant to simulate the speed behavoir of the stepper motor drivers."""
@@ -15,23 +13,17 @@ class StepperModel(object):
 	parameters = {
 		"ACC": 1,
 		"DEC": 1,
-		"MAX_SPEED": 10,
-		"MIN_SPEED": 1,
+		"MAX_SPEED": 50,
+		#"MIN_SPEED": 1,
 	}
 	
 	mode = "idle"
 	target = 0
 	
+	_modes = {"idle","move","run"}
+	
 	def simulate(self,steps=1):
 		for step in range(steps):
-			#if self.mode=="move":
-				#acceleration=direction*self.parameters["ACC"]
-				#if self._will_overshoot():
-					#acceleration=-self.parameters["DEC"]
-				#if self.target==self.position and self.velocity==0:
-					#self.mode=="idle"
-			#if self.mode=="idle":
-				#acceleration=0
 			if self.mode=="move":
 				direction=self.move_direction
 				overshoot=self._overshoot()
@@ -39,18 +31,22 @@ class StepperModel(object):
 					[-self.parameters["DEC"],0,self.parameters["ACC"]],
 					key=lambda choice: abs(overshoot+choice*direction)
 				)*direction
-				if self.target==self.position and self.velocity==0:
+				if self._almost_there():
 					self.mode="idle"
+					self.position=self.target
+					self.velocity=0
 			if self.mode=="idle":
 				acceleration=0
 			
 			if abs(self.velocity)>self.parameters["MAX_SPEED"]:
 				self.velocity=math.copysign(self.parameters["MAX_SPEED"],self.velocity)
-			#if (abs(self.velocity)<self.parameters["MIN_SPEED"]) and acceleration<=0:
-				#self.velocity=0
 			
 			self.velocity+=acceleration
 			self.position+=self.velocity
+	
+	def _almost_there(self):
+		lowest_step_possible=max(self.parameters["ACC"],self.parameters["DEC"])
+		return abs(self.target-self.position)<lowest_step_possible and self.velocity<lowest_step_possible
 	
 	def _overshoot(self):
 		distance_to_go=self.target-(self.position+self.velocity)
@@ -75,13 +71,11 @@ class StepperModel(object):
 		return "%s %d%+d" % (self.__class__.__name__,self.position,self.velocity)
 
 if __name__=="__main__":
-	s=StepperModel()
-	s.move(40)
-	#print(s)
-	
-	def test_simulation(steps=30):
+	def test_simulation(distance=110,steps=100):
+		s=StepperModel()
+		s.move(1100)
+		print(s)
 		for i in range(steps):
-			time.sleep(0.02)
 			s.simulate(1)
 			print(s.mode,s,)
 			if s.mode=="idle":
@@ -91,12 +85,11 @@ if __name__=="__main__":
 		for i in r:
 			s=StepperModel()
 			s.move(i)
-			for step in range(abs(i)+10):
+			for step in range(abs(i)+13):
 				s.simulate(1)
 				if s.mode=="idle":
 					break
 			print(s.position==i,s.mode=="idle",s.position,i,s.mode,step)
 	
-	test_stopping()
-	
-	#test_simulation()
+	#test_stopping()
+	test_simulation(1100)
