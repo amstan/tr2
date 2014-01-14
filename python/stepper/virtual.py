06c9f1881ee5e4913949b1dfc09c32d7be8a2e92
Alexandru Stan <alex@hypertriangle.com>
2014-01-13 19:28:32 -0500

    seems to work in both directions now

--- python/stepper/virtual.py
+++ python/stepper/virtual.py
@@ -16,7 +16,7 @@ class StepperModel(object):
 		"ACC": 1,
 		"DEC": 1,
 		"MAX_SPEED": 10,
-		"MIN_SPEED": 0,
+		"MIN_SPEED": 1,
 	}
 	
 	mode = "idle"
@@ -25,7 +25,6 @@ class StepperModel(object):
 	def simulate(self,steps=1):
 		for step in range(steps):
 			#if self.mode=="move":
-				#direction=(1,-1)[self.target<self.position]
 				#acceleration=direction*self.parameters["ACC"]
 				#if self._will_overshoot():
 					#acceleration=-self.parameters["DEC"]
@@ -34,28 +33,35 @@ class StepperModel(object):
 			#if self.mode=="idle":
 				#acceleration=0
 			if self.mode=="move":
+				direction=self.move_direction
+				overshoot=self._overshoot()
 				acceleration=min(
 					[-self.parameters["DEC"],0,self.parameters["ACC"]],
-					key=lambda choice: abs(self._overshoot()+choice)
-				)
+					key=lambda choice: abs(overshoot+choice*direction)
+				)*direction
 				if self.target==self.position and self.velocity==0:
 					self.mode="idle"
 			if self.mode=="idle":
 				acceleration=0
 			
-			self.velocity+=acceleration
-			self.position+=self.velocity
-			
 			if abs(self.velocity)>self.parameters["MAX_SPEED"]:
 				self.velocity=math.copysign(self.parameters["MAX_SPEED"],self.velocity)
-			if (abs(self.velocity)<self.parameters["MIN_SPEED"]) and acceleration<=0:
-				self.velocity=0
+			#if (abs(self.velocity)<self.parameters["MIN_SPEED"]) and acceleration<=0:
+				#self.velocity=0
+			
+			self.velocity+=acceleration
+			self.position+=self.velocity
 	
 	def _overshoot(self):
 		distance_to_go=self.target-(self.position+self.velocity)
-		steps_to_go=self.velocity/self.parameters["DEC"]
-		stopping_distance=((self.velocity+self.parameters["ACC"])*steps_to_go)/2
+		direction=self.move_direction
+		steps_to_go=self.velocity/(self.parameters["DEC"]*direction)
+		stopping_distance=((self.velocity+(self.parameters["ACC"]*direction))*steps_to_go)/2
 		return stopping_distance-distance_to_go
+	
+	@property
+	def move_direction(self):
+		return (1,-1)[self.target<self.position]
 	
 	#def run(self,target_velocity):
 		#mode="run"
@@ -70,24 +76,26 @@ class StepperModel(object):
 
 if __name__=="__main__":
 	s=StepperModel()
-	s.move(4)
+	s.move(40)
 	#print(s)
 	
 	def test_simulation(steps=30):
 		for i in range(steps):
 			time.sleep(0.02)
 			s.simulate(1)
-			print(s.mode,s._overshoot(),s,)
+			print(s.mode,s,)
 			if s.mode=="idle":
 				break
 	
-	def test_stopping(r=range(0,120)):
+	def test_stopping(r=range(-120,120)):
 		for i in r:
 			s=StepperModel()
 			s.move(i)
-			for step in range(i+10):
+			for step in range(abs(i)+10):
 				s.simulate(1)
-			print(s.position==i,s.mode=="idle",s.position,i,s.mode)
+				if s.mode=="idle":
+					break
+			print(s.position==i,s.mode=="idle",s.position,i,s.mode,step)
 	
 	test_stopping()
 	
