d54dc6d1be44a7e15716d5cde8dff34f38fbbd59
Andrew Rossignol <andrew.rossignol@gmail.com>
2014-02-08 10:57:34 -0500

    Blue LED!
    This commit enables the blue LED on the STM32F4DISCOVERY board.

--- firmware/main.c
+++ firmware/main.c
@@ -1,5 +1,27 @@
+/*
+ * Base C file for the STM32F4 Bare Metal Bring Up
+ */
+
+#include <stdint.h>
+
+#define RCC_BASE 0x40023800
+#define RCC_AHB1ENR *((uint32_t *)(RCC_BASE + 0x30))
+
+#define RCC_GPIOD 3
+
+#define PORTD_BASE 0x40020C00
+#define PORTD_MODE *((uint32_t *)(PORTD_BASE + 0x00))
+#define PORTD *((uint32_t *)(PORTD_BASE + 0x14))
+
+#define LED_BLUE 15
+
 int main(void)
 {
+    RCC_AHB1ENR |= (1 << RCC_GPIOD);
+    
+    PORTD_MODE = (1 << (LED_BLUE * 2));
+    PORTD = (1 << LED_BLUE);
+    
     while(1);
     return;
 }
